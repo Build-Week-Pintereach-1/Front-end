9e545deca21c11da2e47231ff299ad12b5069786
Julie <julieantonio38@gmail.com>
2019-08-26 14:56:15 -0400

    added axios, created article list and article card

---
+++ pintereach-fe/src/components/ArticleCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+export default function ArticleCard({ article }) {
+  const { id, title, abstract } = article;
+  return (
+  <div className= 'articleCard' key ={id}>
+    
+    <h2> {title} </h2>
+    
+    <p> {abstract} </p>
+  </div>
+  
+  );
+}
+
+export default ArticleCard;
