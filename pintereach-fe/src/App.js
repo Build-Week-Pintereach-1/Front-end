fb9095e10886aefa7fc2df058f77e21862ffd7b2
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-08-26 20:57:00 -0700

    Login and Signup forms done

--- pintereach-fe/src/App.js
+++ pintereach-fe/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
 
+
 import Header from './components/Header';
 import MainContainer from './components/MainContainer'
+import LoginForm from "./components/Login";
+import SignUpForm from "./components/SignUp";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <MainContainer />
+      <LoginForm />
+      <SignUpForm />
     </div>
   );
 }
