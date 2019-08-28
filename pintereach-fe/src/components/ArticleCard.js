8c952ab3ae784c59a0697889b1982439fdeba502
“marzier” <atton.rand.23@gmail.com>
2019-08-28 13:31:42 -0700

    abstract is abbreviated to the first three sentences

--- pintereach-fe/src/components/ArticleCard.js
+++ pintereach-fe/src/components/ArticleCard.js
@@ -2,8 +2,25 @@ import React from "react";
 
 export default function ArticleCard({ article }) {
 
-
   const { id, abstract, title_display, author_display, journal } = article;
+
+  
+  //////////
+  // abbreviates abstract to the first three sentences...
+  // finds sentences based on the pressence of '. ' 
+
+  let abbreviatedAbstract = abstract[0];
+  // console.log("abstract: ", abbreviatedAbstract);
+  let firstIndex = abbreviatedAbstract.indexOf('. ');
+  // console.log("firstIndex: ", firstIndex);
+
+  let secondIndex = abbreviatedAbstract.indexOf('. ', firstIndex+2);
+  let thirdIndex = abbreviatedAbstract.indexOf('. ', secondIndex+2);
+
+  abbreviatedAbstract = abbreviatedAbstract.slice(0, thirdIndex);
+  abbreviatedAbstract = abbreviatedAbstract + ' . . .';
+  //////////
+
   
   return (
     <div className= 'article-card' key="id" >
@@ -15,7 +32,7 @@ export default function ArticleCard({ article }) {
           {author_display}
       </div>
       <a href={`http://doi.org/${id}`} target='_blank'>View Article</a>
-      <p className="article-abstract">{abstract}</p>   
+      <p className="article-abstract">{abbreviatedAbstract}</p>   
       </div>
       <button className= "save-button" onClick={()=>console.log("clicked")} >
         Save Article
