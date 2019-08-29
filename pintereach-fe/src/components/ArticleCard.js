ed0ad68626a277491da63e7729cec83ee74e54d6
Faye Skeen <fskeen@gmail.com>
2019-08-29 13:38:15 -0700

    Styling tweaks, added overflow hidden to preserve abstract.

--- pintereach-fe/src/components/ArticleCard.js
+++ pintereach-fe/src/components/ArticleCard.js
@@ -19,9 +19,6 @@ export default function ArticleCard({ article }) {
       </div>
       <a href={`http://doi.org/${id}`} target='_blank'>View Article</a>
       <p className="article-abstract">{abstract}</p>   
-      </div>
-      /*This button should maybe post the article to our backend too */
-      <div>
       <button className= "save-button" onClick={toggleModal} >
         Save Article
       </button>
--- pintereach-fe/src/components/ArticleList.js
+++ pintereach-fe/src/components/ArticleList.js
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import ArticleCard from './ArticleCard';
-
+import StackGrid from "react-stack-grid";
 function ArticleList({search}) {
     const [article, setArticle] = useState([]);
-
     useEffect(() => {
-
       axios
-      .get(`https://cors-anywhere.herokuapp.com/http://api.plos.org/search?q=${search}&api_key=z2S-nFcgkr5BxkxKRb2v`)
+      .get(`https://cors-anywhere.herokuapp.com/http://api.plos.org/search?q=${search}&start=1&rows=21&api_key=z2S-nFcgkr5BxkxKRb2v`)
       .then(response => {
         console.log(response.data.response.docs);
         setArticle(response.data.response.docs);
@@ -20,10 +18,13 @@ function ArticleList({search}) {
   
     return (
       <section className="article-list">
-
+           <StackGrid
+              columnWidth={500}
+      >
           {article.map(article => {
-            return <ArticleCard key= {article.id} article = {article}/>;
+            return  <ArticleCard key= {article.id} article = {article}/>;
           })}
+          </StackGrid>
       </section>
     );
 }
